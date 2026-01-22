c275e726baca22dd3452644a50d8e47bde76ed4c
smritijaiswal2007 <smritijais999@gmail.com>
2026-01-22 23:09:46 +0530

    done

--- project/app/views.py
+++ project/app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def my_render(req):
-    x = 10
+    x = 15
     y = 'php'
     z = [1,2,3,4,5]
     return render(req,'index.html',{'p':x,'q':y,'r':z})
